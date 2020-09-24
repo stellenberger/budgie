f6c93200948a6f25ce980ef2cbc52fd77c5c142c
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-09-24 20:24:06 +0100

    Added React Router to the front end

--- client/budgie-frontend/src/App.js
+++ client/budgie-frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import { 
+  BrowserRouter as Router, 
+  Switch,
+  Route
+} from 'react-router-dom'
+import { Landing } from './components'
 
 function App() {
   return (
     <div className="App">
       This is the app component
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/dashboard' component={Landing} />
+          <Route exact path='/statistics' component={Landing} />
+          <Route exact path='/account/:id' component={Landing} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/budgie-frontend/src/components/landing/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      I am the landing component
+    </div>
+  )
+}
+
+export default Landing
