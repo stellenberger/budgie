8d038ce7d0ca68e4ef68b7c1ed3f1457069903ef
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-01 16:04:08 +0100

    Basic login form completed without functionality or styling

--- client/budgie-frontend/src/components/authentication/Login.jsx
+++ client/budgie-frontend/src/components/authentication/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+  const [user, setUser] = useState({username: '', password: ''})
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log('form submitted', user)
+  }
+
+  const handleOnChange = (e) => {
+    setUser({...user, [e.target.name]: e.target.value})
+    console.log('Handeling on change', user)
+  }
+
   return (
     <div>
-      I am the Login Component
+      <form action="POST" onSubmit={submitForm}>
+        <input type="text" name="username" onChange={handleOnChange} value={user.username}/>
+        <input type="password" name="password" onChange={handleOnChange} value={user.password}/>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
