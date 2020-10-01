54e8d80ae6add46d09f6598b7be01e5a1662ac0f
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-01 16:09:03 +0100

    Updated the register component with simple form

--- client/budgie-frontend/src/components/authentication/Register.jsx
+++ client/budgie-frontend/src/components/authentication/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Register() {
+  const [user, setUser] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    username: '', 
+    password: '',
+    password_confirmation: ''
+  })
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log('register submitted', user)
+  }
+
+  const handleOnChange = (e) => {
+    setUser({...user, [e.target.name]: e.target.value})
+    console.log('Handeling on change', user)
+  }
+
   return (
     <div>
-      I am the Register Component
+      <form action="POST" onSubmit={submitForm}>
+        <input type="text" name="firstname" onChange={handleOnChange} value={user.firstname}/>
+        <input type="text" name="lastname" onChange={handleOnChange} value={user.lastname}/>
+        <input type="email" name="email" onChange={handleOnChange} value={user.email}/>
+        <input type="text" name="username" onChange={handleOnChange} value={user.username}/>
+        <input type="password" name="password" onChange={handleOnChange} value={user.password}/>
+        <input type="password" name="password_confirmation" onChange={handleOnChange} value={user.password_confirmation}/>
+        <input type="submit"/>
+      </form>
     </div>
   )
 }
+
