4fbda4316201c479cbc8e4eb99cf2f0b77aedcde
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-02 16:01:54 +0100

    Changed register state to loginuser

--- client/budgie-frontend/src/components/authentication/Register.jsx
+++ client/budgie-frontend/src/components/authentication/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styles from './Authentication.module.scss'
 
 export default function Register() {
-  const [user, setUser] = useState({
+  const [loginUser, setLoginUser] = useState({
     firstname: '',
     lastname: '',
     email: '',
@@ -13,12 +13,12 @@ export default function Register() {
 
   const submitForm = (e) => {
     e.preventDefault()
-    console.log('register submitted', user)
+    console.log('register submitted', loginUser)
   }
 
   const handleOnChange = (e) => {
-    setUser({...user, [e.target.name]: e.target.value})
-    console.log('Handeling on change', user)
+    setLoginUser({...loginUser, [e.target.name]: e.target.value})
+    console.log('Handeling on change', loginUser)
   }
 
   return (
@@ -27,17 +27,17 @@ export default function Register() {
         <h2>Register here!</h2>
         <form action="POST" onSubmit={submitForm}>
           <label>First Name</label> <br/>
-          <input type="text" name="firstname" onChange={handleOnChange} value={user.firstname}/><br/>
+          <input type="text" name="firstname" onChange={handleOnChange} value={loginUser.firstname}/><br/>
           <label>Last Name</label><br/>
-          <input type="text" name="lastname" onChange={handleOnChange} value={user.lastname}/><br/>
+          <input type="text" name="lastname" onChange={handleOnChange} value={loginUser.lastname}/><br/>
           <label>Email</label><br/>
-          <input type="email" name="email" onChange={handleOnChange} value={user.email}/><br/>
+          <input type="email" name="email" onChange={handleOnChange} value={loginUser.email}/><br/>
           <label>Username</label><br/>
-          <input type="text" name="username" onChange={handleOnChange} value={user.username}/><br/>
+          <input type="text" name="username" onChange={handleOnChange} value={loginUser.username}/><br/>
           <label>Password</label><br/>
-          <input type="password" name="password" onChange={handleOnChange} value={user.password}/><br/>
+          <input type="password" name="password" onChange={handleOnChange} value={loginUser.password}/><br/>
           <label>Confirm your password</label><br/>
-          <input type="password" name="password_confirmation" onChange={handleOnChange} value={user.password_confirmation}/><br/>
+          <input type="password" name="password_confirmation" onChange={handleOnChange} value={loginUser.password_confirmation}/><br/>
           <input type="submit"/>
         </form>
       </div>
