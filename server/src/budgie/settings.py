25cf51d5585cce2e5931e133438b7c7f78151b73
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-02 16:39:33 +0100

    added authentication to the application

--- server/src/budgie/settings.py
+++ server/src/budgie/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'budgie.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 
 # Database
--- server/src/budgie/urls.py
+++ server/src/budgie/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core.views import TestView
-
+from rest_framework.authtoken import views
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name='test')
+    path('', TestView.as_view(), name='test'),
+    path('login/', views.obtain_auth_token, name='api-token-auth')
 ]
