b1ab1058531f06d0a3fb4c886b2f1012bbb96fef
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-15 09:18:25 +0100

    Added the user id to the bank account model

--- server/src/apps/bank_accounts/models.py
+++ server/src/apps/bank_accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from apps.utils.models import Timestamps
+from django.contrib.auth.models import User
 
 class BankAccount(Timestamps, models.Model):
   name = models.CharField(max_length=100)
   records = models.FileField()
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
