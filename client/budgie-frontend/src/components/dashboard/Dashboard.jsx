9c690d5082b5288e75c6a24f89112d61d98b1f0f
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-16 13:37:33 +0100

    Dashboard now displays the bank account overview of all bank accounts for the current user

--- client/budgie-frontend/src/components/dashboard/Dashboard.jsx
+++ client/budgie-frontend/src/components/dashboard/Dashboard.jsx
@@ -3,24 +3,23 @@ import { Redirect } from 'react-router-dom'
 import { NavPanel, BankAccount } from '../index.jsx'
 import styles from './Dashboard.module.scss'
 import axios from 'axios'
-import { baseURL, accountEndpoint } from '../../constants'
+import { baseURL, bankAccountEndpointAll } from '../../constants'
 
 
 export default function Dashboard({ user }) {
-  const [account, setAccount] = useState(null)
-  const [accountData, setaccountData] = useState(null)
-
+  const [accounts, setAccounts] = useState([])
+  let accountsObject = []
   useEffect(() => {
-    axios.get(baseURL + accountEndpoint + '5/')
+    axios.get(baseURL + bankAccountEndpointAll + '1/')
     .then((response) => {
-      console.log('successful response', response)
-      setaccountData(response.data[0])
-      setAccount(response.data[1])
+      console.log('successful response', response.data)
+      setAccounts(response.data)
     })
     .catch((error) => {
       console.log('something went wrong: ', error)
     })
   }, [])
+
 
   return (
     <div className={styles.container}>
@@ -30,7 +29,9 @@ export default function Dashboard({ user }) {
         { user && <h1 className={styles.welcomeMessage}>Welcome back, {user.username}!</h1> }
         <h3>This is your Dashboard, where you can find an overview of your spendings over the last few days</h3>
         <h2>Each of your accounts, graphed by date</h2>
-        {account && <BankAccount account={account} accountData={accountData} />}
+        {accounts && accounts.map(account => {
+          return <BankAccount account={account[1]} accountData={account[0]} />
+        })}
       </div>
     </div>
   )
