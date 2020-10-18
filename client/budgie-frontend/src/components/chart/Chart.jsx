4c1a75364051bdf719a40d0888b4bc2778055218
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-18 17:17:19 +0100

    Testing the chart component and adding error handling

--- client/budgie-frontend/src/components/chart/Chart.jsx
+++ client/budgie-frontend/src/components/chart/Chart.jsx
@@ -3,37 +3,43 @@ import {Bar, Line, Pie} from 'react-chartjs-2'
 
 export default function Chart({ account, accountData }) {
   const [chartData, setChartData] = useState(null) 
+  const [errorMessage, setErrorMessage] = useState(null) 
   let dates = []
   let accountBalances = []
   let accountBalance
   const randomNumber = () => {return Math.floor(Math.random() * 255)}
   useEffect(() => {
-    // this will move all dates into their own array
-    for (let i=1; i < accountData.length; i++) {
-      dates.push(accountData[i][0])
+    if (!account || !accountData) {
+      setErrorMessage('Sorry, but we couldnt create your chart! Have you uploaded data?')
+    } else {
+      // this will move all dates into their own array
+      for (let i=1; i < accountData.length; i++) {
+        dates.push(accountData[i][0])
+      }
+      for (let i=1; i < accountData.length; i++) {
+        accountBalance = parseInt(accountData[i][7])
+        accountBalances.push(accountBalance)
+      }
+      setChartData({
+        labels: dates.reverse(),
+        datasets: [
+          {
+            label: account.name,
+            backgroundColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},0.2)`,
+            borderColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},1)`,
+            borderWidth: 1,
+            hoverBackgroundColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},0.4)`,
+            hoverBorderColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},1)`,
+            data: accountBalances.reverse()
+          }
+        ] 
+      })
     }
-    for (let i=1; i < accountData.length; i++) {
-      accountBalance = parseInt(accountData[i][7])
-      accountBalances.push(accountBalance)
-    }
-    setChartData({
-      labels: dates.reverse(),
-      datasets: [
-        {
-          label: account.name,
-          backgroundColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},0.2)`,
-          borderColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},1)`,
-          borderWidth: 1,
-          hoverBackgroundColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},0.4)`,
-          hoverBorderColor: `rgba(${randomNumber()},${randomNumber()},${randomNumber()},1)`,
-          data: accountBalances.reverse()
-        }
-      ] 
-    })
   }, [])
   
   return (
     <div>
+      <h2>{ errorMessage }</h2>
       {chartData && 
         <div>
           <h2>{account.name}</h2>
--- client/budgie-frontend/src/components/chart/__tests__/Chart.test.js
+++ client/budgie-frontend/src/components/chart/__tests__/Chart.test.js
@@ -4,9 +4,27 @@ import { render, fireEvent } from '@testing-library/react'
 
 describe('Testing the Chart Component', () => {
   let container
-  
-  it('renders without crashing', () => {
+  let account = {name: 'Debit account example'}
+  let accountData = [
+    ["Transaction Date", "Transaction Type", "Sort Code", "Account Number", "Transaction Description", "Debit Amount", "Credit Amount", "Balance"],
+    ["28/01/2020", "DD", "11-22-33", "1122334", "American Express", "200.00", "", "1499.00"]
+  ]
+
+  it('renders without crashing, and with no props', () => {
     container = render(<Chart />)
     expect(container).toBeTruthy()
+  })
+
+  it('renders with props passed to it', () => {
+    container = render(<Chart account={account} accountData={accountData}/>)
+    expect(container).toBeTruthy()
+  })
+
+  it('show an error message if there is no account data', async () => {
+    accountData = null
+    account = null
+    const { getByRole } = render(<Chart account={account} accountData={accountData} />)
+    const errorElement = getByRole('heading', {name: /Sorry, but we couldnt create your chart!/i})
+    expect(errorElement).toBeInTheDocument();
   })
 })
