0f0d53206514afcaae2352ee3a6a2caa1cb29bfe
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-10-18 18:23:49 +0100

    More tests added to the NavPanel component

--- client/budgie-frontend/src/components/navpanel/NavPanel.jsx
+++ client/budgie-frontend/src/components/navpanel/NavPanel.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './NavPanel.module.scss'
 import { Link } from 'react-router-dom'
 
+
 export default function NavPanel({ user }) {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    if(user) {
+      return
+    } else {
+      setErrorMessage('Sorry, you need to be logged in to see the navbar')
+    }
+  }, [])
   return (
-    <div className={styles.container}>
-        <h2>{user.username}</h2>
-      <ul>
-        <li><Link to='/'>Statistics</Link></li>
-        <li><Link to='/add_bank_account'>Add new card</Link></li>
-        <li><Link to='/'>Account</Link></li>
-        <li><Link to='/'>About this app</Link></li>
-      </ul>
-    </div>
+    <>
+      {
+        user ? (
+          <div className={styles.container}>
+            <h2>{user.username}</h2>
+            <ul>
+              <li><Link to='/'>Statistics</Link></li>
+              <li><Link to='/add_bank_account'>Add new card</Link></li>
+              <li><Link to='/'>Account</Link></li>
+              <li><Link to='/'>About this app</Link></li>
+            </ul>  
+          </div>
+        ) : (
+          <h2>{ errorMessage }</h2>
+        )
+      }
+        
+    </>
   )
 }
--- client/budgie-frontend/src/components/navpanel/__tests__/NavPanel.test.js
+++ client/budgie-frontend/src/components/navpanel/__tests__/NavPanel.test.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import NavPanel from '../NavPanel'
 import { render, fireEvent } from '@testing-library/react'
+import { 
+  BrowserRouter as BR,
+  Route as R
+} from 'react-router-dom'
 
 describe('Testing the NavPanel Component', () => {
   let container
-  
+  let user = {username: 'stephan', token: '1234'}
+
   it('renders without crashing', () => {
-    container = render(<NavPanel />)
+    container = render(
+      <BR>
+        <NavPanel user={user}/>
+      </BR>
+    )
     expect(container).toBeTruthy()
   })
 
   it('has a title on the page', () => {
-    const { getByText } = render(<NavPanel />);
-    const linkElement = getByText(/I am the NavPanel component/i);
-    expect(linkElement).toBeInTheDocument();
+    const { getByRole, getAllByRole } = render(
+      <BR>
+        <NavPanel user={user}/>
+      </BR>
+    );
+    const titleName = getByRole('heading', {name: /stephan/i});
+    const links = getAllByRole('list');
+    expect(titleName).toBeInTheDocument();
+    expect(links[0]).toHaveTextContent(/Statistics/i)
+    expect(links[0]).toHaveTextContent(/Add new card/i)
+    expect(links[0]).toHaveTextContent(/Account/i)
+    expect(links[0]).toHaveTextContent(/About this app/i)
+  })
+
+  it('still render if there is no user', () => {
+    const { container, getByRole } = render(
+      <BR>
+        <NavPanel />
+      </BR>
+    );
+    expect(container).toBeTruthy()
+  })
+
+  it('will show an erro message if there is no user', () => {
+    const { getByRole } = render(
+      <BR>
+        <NavPanel />
+      </BR>
+    );
+    const errorMessage = getByRole('heading', {name: /Sorry, you need to be logged in to see the navbar/i})
+    expect(errorMessage).toBeTruthy()
   })
 })
